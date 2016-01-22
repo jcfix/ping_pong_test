3b751a569526ba0f24bbdf9b822070a8fc0d68b6
Jessica Fix <jcfix.pdx@gmail.com>
2016-01-22 08:40:51 -0800

    Add passing script for display input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business logic //
 
-// 1st Script for 1st Spec //
+// 1st Script for 1st Spec: Dealing with Invalid Input //
 var errorInput = function(number){
   var output = [];
 
@@ -11,12 +11,8 @@ var errorInput = function(number){
 };
 
 
-//  Script for 1st & 2nd Spec //
-var errorInput = function(number){
-  var output = [];
-
-  if (isNaN(number) || (number === "")) {
-    output.push("error message");
-  }
-  return output;
-};
+//  Script for 2nd Spec: Displaying Numbers //
+function pingPong(number) {
+  var endNumber = number
+  return endNumber;
+}
