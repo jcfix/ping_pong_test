9cdf59925516c4495ae7f5962250c6a4b635436c
Jessica Fix <jcfix.pdx@gmail.com>
2016-01-22 13:18:47 -0800

    Refactor pingPong specs and scripts to include countUp

--- js/scripts.js
+++ js/scripts.js
@@ -11,48 +11,46 @@ var errorInput = function(number){
 };
 
 
-// Script for countUp //
-function countUp(number) {
+// Combines countUp and pingPong functions //
+
+var pingPong = function (number) {
   var numberArray = [];
   for (var i = 1; i <= number; i++) {
-    numberArray.push(i);
+    if (i % 5 === 0 && i % 3 === 0) {
+      numberArray.push("pingpong");
+    }
+    else if (i % 5 === 0) {
+      numberArray.push("pong");
+    }
+    else if (i % 3 === 0) {
+      numberArray.push("ping");
+    }
+    else {
+      numberArray.push(i)
+    }
   }
   return numberArray;
- };
-
-
-
-//  Script for pingPong//
-function pingPong(number) {
-  var endNumber = number
-  if (number % 5 === 0 && number % 3 === 0) {
-    return "pingpong";
-  } else if (number % 5 === 0) {
-  return "pong";
-  } else if ( number % 3 === 0) {
-  return "ping";
-  }
-  return endNumber;
 };
 
 
 
 
 
-User-interface Logic //
-$(document).ready(function() {
-  $(".btn#getNumber").click(function(event){
-    var number = parseInt($("input#userNumber").val());
-    var returnedArray = pingPong(number);
 
-    for (var i = 0; i < returnedArray.length; i++) {
-      var listItem = document.createElement("li");
-      listItem.innerHTML = returnedArray[i];
-      var numberList = document.getElementById("ping pong");
-      numberList.appendChild(listItem);
-    }
-
-    $("#listofNumbers").show();
-    event.preventDefault()
-  });
-});
+// User-interface Logic //
+// $(document).ready(function() {
+//   $(".btn#getNumber").click(function(event){
+//     var number = parseInt($("input#userNumber").val());
+//     var returnedArray = pingPong(number);
+//
+//     for (var i = 0; i < returnedArray.length; i++) {
+//       var listItem = document.createElement("li");
+//       listItem.innerHTML = returnedArray[i];
+//       var numberList = document.getElementById("ping pong");
+//       numberList.appendChild(listItem);
+//     }
+//
+//     $("#listofNumbers").show();
+//     event.preventDefault()
+//   });
+// });
--- specs/specs.js
+++ specs/specs.js
@@ -5,24 +5,55 @@ describe('errorInput', function() {
   });
 });
 
+describe("pingPong", function() {
 
-// countUp Function //
-describe("countUp", function() {
-  it("will count up to a user's number by 1 starting at 1 and put the numbers into an array", function() {
-    expect(countUp(2)).to.eql([1,2]);
+  it("will count up to the user's number in multiples of 1, starting from 1, and it will put the numbers into an array", function () {
+    expect(pingPong(2)).to.eql([1,2]);
   });
+
+  it("will return pingpong for numbers divisible by 3 and 5 in the array", function (){
+    expect(pingPong(15)).to.eql([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong"]);
+  });
+
+  it("will return pong for numbers divisible by 5 in the array", function() {
+    expect(pingPong(5)).to.eql([1,2,"ping",4,"pong"]);
+  });
+
+  it("will return ping for numbers divisible by 3 in the array", function() {
+    expect(pingPong(3)).to.eql([1,2,"ping"]);
+  });
+
+
 });
 
 
-// pingPong Function //
-describe('pingPong', function() {
-  it ("will replace numbers divisible by 3 and 5 in the array with pingpong", function() {
-    expect(pingPong(15)).to.equal("pingpong");
-  });
-  it ("will replace numbers divisible by 5 in the array with pong", function() {
-    expect(pingPong(5)).to.equal("pong");
-  });
-  it ("will replace numbers divisible by 3 in the array with ping", function() {
-    expect(pingPong(3)).to.equal("ping");
-  });
-});
+
+
+
+
+
+
+
+
+
+
+// // countUp Function //
+// describe("countUp", function() {
+//   it("will count up to a user's number by 1 starting at 1 and put the numbers into an array", function() {
+//     expect(countUp(2)).to.eql([1,2]);
+//   });
+// });
+//
+//
+// // pingPong Function //
+// describe('pingPong', function() {
+//   it ("will replace numbers divisible by 3 and 5 in the array with pingpong", function() {
+//     expect(pingPong(15)).to.equal("pingpong");
+//   });
+//   it ("will replace numbers divisible by 5 in the array with pong", function() {
+//     expect(pingPong(5)).to.equal("pong");
+//   });
+//   it ("will replace numbers divisible by 3 in the array with ping", function() {
+//     expect(pingPong(3)).to.equal("ping");
+//   });
+// });
