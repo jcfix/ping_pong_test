9189e5f14ae41b1aadc370e46110f5606f027897
Jessica Fix <jcfix.pdx@gmail.com>
2016-01-22 08:39:17 -0800

    Add spec for display input number

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,14 @@ var errorInput = function(number){
   }
   return output;
 };
+
+
+//  Script for 1st & 2nd Spec //
+var errorInput = function(number){
+  var output = [];
+
+  if (isNaN(number) || (number === "")) {
+    output.push("error message");
+  }
+  return output;
+};
--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,7 @@ describe('errorInput', function() {
   it("will handle empty space input errors with an error message", function(){
     expect(errorInput("")).to.eql(["error message"]);
   });
+  it ("will display the number that is entered by the user", function() {
+    expect(pingPong(25)).to.equal(25);
+  });
 });
