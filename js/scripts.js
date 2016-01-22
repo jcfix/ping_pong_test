2ef1fa94e7780a6ecb8691eb21126f54c0acd1ca
Jessica Fix <jcfix.pdx@gmail.com>
2016-01-22 09:10:16 -0800

    Change display spec to countTo spec

--- js/scripts.js
+++ js/scripts.js
@@ -12,24 +12,39 @@ var errorInput = function(number){
 
 
 //  Script for 2nd Spec: Displaying userNumber //
-// function pingPong(number) {
-//   var endNumber = number
-//   return endNumber;
-// }
+function pingPong(number) {
+  var endNumber = number
+  return endNumber;
+}
 
 
 // Script for 3rd Spec: Displaying list of numbers //
-function pingPong(number) {
-  var endNumber = number
+// function pingPong(number) {
+//   var endNumber = number
+//
+//   for (var i = 1; i <= number; i++) {
+//
+//     $("#listofNumbers").append("<li>" + i + "</li>");
+//
+//   }
+//   return number;
+//
+// };
 
-  for (var i = 1; i <= number; i++) {
 
-    $("#listofNumbers").append("<li>" + i + "</li>");
-
-  }
-  return number;
-
-};
-
-
-// Script for 4th Spec: Replace number % 3 & 5 with "ping pong"
+// Script for 4th Spec: Replace numbers % 3 & 5 with "ping pong"//
+// function pingPong(number) {
+//   var endNumber = number
+//
+//   for (var i = 1; i <= number; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       $("#listofNumbers").append("<li>" + "ping pong" + "</li>");
+//     }
+//
+//
+//     $("#listofNumbers").append("<li>" + i + "</li>");
+//
+//   }
+//   return number;
+//
+// }
--- specs/specs.js
+++ specs/specs.js
@@ -1,17 +1,42 @@
+//  errorInput Function//
 describe('errorInput', function() {
   it("will handle empty space input errors with an error message", function(){
     expect(errorInput("")).to.eql(["error message"]);
   });
 });
 
+
+// pingPong Function //
 describe('pingPong', function(){
   it ("will display the number that is entered by the user", function() {
     expect(pingPong(25)).to.equal(25);
   });
-  it ("will display the sequence of numbers starting with 1 and ending with the number that is entered by the user", function() {
-    expect(pingPong(50)).to.equal(50);
-  });
-  it ("will replace numbers divisible by three and five with 'ping pong'", function() {
-    expect(pingPong(15)).to.equal('ping pong');
+});
+
+
+// countTo Function //
+describe('countTo', function(){
+  it("will count to the user's number by starting at 1, and in increments of 1, and will display the sequence in an array", function() {
+    expect(countTo(6)).to.equal(1,2,3,4,5,6);
   });
 });
+
+//   it ("will display the sequence of numbers starting with 1 and ending with the number that is entered by the user", function() {
+//     expect(pingPong(50)).to.equal(50);
+//   });
+//
+//
+//   it ("will replace numbers divisible by three and five with 'ping pong'", function() {
+//     expect(pingPong(15)).to.equal("ping pong");
+//   });
+// });
+
+
+//  Logic //
+// If a number between 1-userInput % 3 === 0 //
+  // Return "ping" //
+  // Else if a number between 1-userInput % 5 === 0 //
+  // Return "pong" //
+  // Else if a number between 1-userInput % 15 === 0 //
+  // Return "ping pong" //
+// Else, return number //
